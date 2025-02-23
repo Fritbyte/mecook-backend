49549a8cfa077bff8d6052080035e6aec3cff126
Fritbyte <softly2200@gmail.com>
2025-02-23 00:00:06 -0800

    Init commit

---
+++ src/main/java/com/mecook/mecookbackend/MeCookApplication.java
@@ -0,0 +1,13 @@
+package com.mecook.mecookbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeCookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeCookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mecook/mecookbackend/MeCookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mecook.mecookbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeCookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
